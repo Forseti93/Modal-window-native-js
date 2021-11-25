16118e514fb98f0faef312281f9dd3ad706aa6be
Sergey <sergeylavrenov93@gmail.com>
2021-11-25 18:56:36 +0200

    timer added

--- js/script.js
+++ js/script.js
@@ -1,76 +1,20 @@
-/* //ожидание загружки виндов. весь код для страницы писать в эту функцию
 window.addEventListener("DOMContentLoaded", function () {
   "use strict";
-  //#region tabs
-  //контейнер табов
-  let tabsContainer = document.querySelector(".info-header");
-  //табы
-  let tabs = document.querySelectorAll(".info-header-tab");
-  //контент табов
-  let tabContent = document.querySelectorAll(".info-tabcontent");
-  //для таб-контентов начиная с номера таба с номером а: 
-  //добавить класс "hide", убрать "show"
-  function hideTabContent(a) {
-    //для обращения к каждому емументу tabContent(object), используем for()
-    for (let i = a; i < tabContent.length; i++) {
-      tabContent[i].classList.remove("show");
-      tabContent[i].classList.add("hide");
-    }
-  }
-  //скрываем всё после номера 1
-  hideTabContent(1);
-  //показать таб-контент для таба tabContent[b]
-  function showTabContent(b) {
-    //проверяем, что таб-контент спрятан
-    if (tabContent[b].classList.contains("hide")) {
-      tabContent[b].classList.remove("hide");
-      tabContent[b].classList.add("show");
-    }
-  }
-  //следим за нажатиями на табы,
-  //прячем все табы hideTabContent(0)
-  //показываем табконтент нажатого таба showTabContent(i)
-  tabsContainer.addEventListener("click", function (event) {
-    //храним нажатый объект
-    let target = event.target;
-    if (target && target.classList.contains("info-header-tab")) {
-      for (let i = 0; i < tabs.length; i++) {
-        //если нажатый объект совпадает с табом tabs[i]...
-        if (target == tabs[i]) {
-          hideTabContent(0);
-          showTabContent(i);
-          break;
-        }
-      }
-    }
-  });
-  //#endregion tabs
-});
- */
 
-/*ждём загрузки дома
-объявили элементы хтмл
-функция спрятать всё кроме 1 табконтента
-функция показать нажатый
-обработчик клика по табам*/
-
-window.addEventListener("DOMContentLoaded", function () {
-  "use strict";
+  //#region Tabs
   let tabsContainer = document.querySelector(".info-header");
   let tabs = document.querySelectorAll(".info-header-tab");
   let tabContent = document.querySelectorAll(".info-tabcontent");
-
   //функция тоглит класс visible у табконтента
   function hideShowTabcontent(showTabcontent) {
     for (let i = 0; i < tabs.length; i++) {
-        tabContent[i].classList.remove("visible");
+      tabContent[i].classList.remove("visible");
       if (showTabcontent === i) {
         tabContent[i].classList.toggle("visible");
       }
     }
   }
   hideShowTabcontent(0); //какой таб показать (от 0)
-
   //тоглим класс при клике на табы
   tabsContainer.addEventListener("click", function (e) {
     let target = e.target;
@@ -83,4 +27,53 @@ window.addEventListener("DOMContentLoaded", function () {
       }
     });
   });
+  //#endregion tabs
+
+  //#region timer
+  //Set deadline, timer end date in format YYYY-MM-DD
+  const timerUntil = "2021-11-28";
+  //array of timer's HTML elements
+  const timerComponents = [
+    document.querySelector(".timer-numbers>.hours"),
+    document.querySelector(".timer-numbers>.minutes"),
+    document.querySelector(".timer-numbers>.seconds"),
+  ];
+  //Calculates remaining time in milliseconds and returns object
+  //deadline - end of countdown
+  function remainingTime(deadline) {
+    const dateNow = Date.parse(Date());
+    const remainingTimeMS = Date.parse(deadline) - dateNow;
+    const hours = Math.floor((remainingTimeMS / (1000 * 60 * 60 * 24)) % 24);
+    const minutes = Math.floor((remainingTimeMS / (1000 * 60)) % 60);
+    const seconds = Math.floor((remainingTimeMS / 1000) % 60);
+    return {
+      remainingTime: remainingTimeMS,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+  //function assigns innerText to timer
+  //arguments of function are: (HTML elements of timer) and (object from function remainingTime)
+  function loopAndReplace(timerUnits, timerValues) {
+    const objValuesArray = Object.values(timerValues);
+    for (let i = 1; i < objValuesArray.length; i++) {
+      if (objValuesArray[i] / 10 < 1) {
+        timerUnits[i - 1].innerText = `0${objValuesArray[i]}`;
+      } else {
+        timerUnits[i - 1].innerText = objValuesArray[i];
+      }
+    }
+  }
+  //to show countdown after page loaded
+  loopAndReplace(timerComponents, remainingTime(timerUntil));
+  //interval that starts functions evey second
+  const second = setInterval(function () {
+    loopAndReplace(timerComponents, remainingTime(timerUntil));
+  }, 1000);
+  //clear interval second when coundown is <= 0
+  if (remainingTime(timerUntil).remainingTime <= 0) {
+    clearInterval(second);
+  }
+  //#endregion timer
 });
